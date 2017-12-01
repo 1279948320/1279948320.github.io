f887273771266fd9534e9294c947620cc4ddb1a1
1279948320 <1279948320@qq.com>
2017-12-01 15:07:20 +0800

    Add files via upload

---
+++ baidu/src/components/vue.js
@@ -0,0 +1,182 @@
+
+var app1 = new Vue({
+    el:"#app1",
+    data:{
+    lists:[
+            {name:"推荐",url:"page.html"},
+           {name:"视频",url:""},
+           {name:"热点",url:""},
+           {name:"娱乐",url:""},
+           {name:"时尚",url:""},
+          {name:"+",url:""}
+          ]
+}
+})
+
+
+
+var app2 = new Vue({
+    el:"#app2",
+    data:{
+        imgUrl:"img/1.png"
+    }
+})
+
+
+
+var app4 = new Vue({
+    el:"#app4",
+    data:{
+    img1:"img/2.png",
+    img2:"img/3.png",
+    img3:"img/4.png",
+    img4:"img/5.png",
+    img5:"img/6.png",
+    img6:"img/7.png"
+}
+})
+
+var app6 =new Vue({
+    el:"#app6",
+    data:{
+        message:"习近平用5句话介绍中国“新时代”",
+        text:"央广网 11-19",
+        tit:"置顶"
+    }
+})
+
+
+var app7 =new Vue({
+    el:"#app7",
+    data:{
+        message:"农村大集鲫鱼鲤鱼铺满地，鱼的来源让人太生气，最好别吃",
+        text:"河北乡土 11-19",
+        tit:"图集",
+        img1:"img/9.png",
+        text1:"打开手机百度"
+    }
+})
+
+
+var app8 =new Vue({
+    el:"#app8",
+    data:{
+        message:"女人情到深处时，才会这样称呼你，你可曾被如此称呼过？",
+        text:"宇宙用户精选 11-16",
+        tit:"图集",
+        text1:"打开手机百度"
+    }
+})
+
+
+var app9 =new Vue({
+    el:"#app9",
+    data:{
+        message:"超神学院：曾经的雄兵连终于集合了，真是不容易啊！",
+        text:"透明的车玻璃 11-18",
+        img1:"img/10.png",
+        text1:"打开手机百度"
+    }
+})
+
+
+var app10 =new Vue({
+    el:"#app10",
+    data:{
+        message:"烤鱼店生意红火 秘方：用漏斗过滤剩菜后的口水油",
+        text:"南方都市报 07-27",
+        tit:"热点",
+        
+    }
+})
+
+
+
+var app11 =new Vue({
+    el:"#app11",
+    data:{
+        message:"刘鑫称她也不记得当时有没有反锁门，现在是百口莫辩解释不清楚了",
+        text:"娱七杂把话天下 11-19",
+        img1:"img/11.png",
+        text1:"打开手机百度"
+    }
+})
+
+
+var app12 =new Vue({
+    el:"#app12",
+    data:{
+        message:"结婚70周年！英国女王庆祝白金婚 王室发布合影",
+        text:"新浪娱乐 11-19"
+    }
+})
+
+
+
+var app13 =new Vue({
+    el:"#app13",
+    data:{
+        message:"女兵在阅兵时想上厕所怎么办，原来还有这种操作",
+        text:"军人魂 11-18"
+    }
+})
+
+
+
+var app14 =new Vue({
+    el:"#app14",
+    data:{
+        message:"专家发现：女人有没有生过孩子，看下面就知道。非常“与众不同”",
+        text:"只要你愿意等 11-19"
+    }
+})
+
+
+
+
+var app15 =new Vue({
+    el:"#app15",
+    data:{
+        message:"日本女星Shinozaki沙滩游玩，太漂亮了",
+        text:"精选影片 11-17",
+        tit:"图集",
+        img1:"img/12.png",
+        text1:"打开手机百度"
+    }
+})
+
+
+
+var app16 =new Vue({
+    el:"#app16",
+    data:{
+        message:"农村孤寡老人没钱装暖气，施工人员牵走他家的羊炖了，还拆掉壁挂炉",
+        text:"闻舞视界 11-19",
+        tit:"图集",
+        text1:"打开手机百度"
+    }
+})
+
+
+var app17 =new Vue({
+    el:"#app17",
+    data:{
+        message:"马未都：我为什么不佩戴文玩，说出来你们不要觉得恶心",
+        text:"百家人和文 11-18",
+        img1:"img/13.png",
+        text1:"打开手机百度"
+    }
+})
+
+
+
+var app18 = new Vue({
+    el:"#app18",
+    data:{
+        message:"用户反馈",
+        img1:"img/14.png",
+        img2:"img/15.png",
+        text:"使用百度前必读 Baidu 京IPC证030173号",
+        tit:"京公安网安备11000002000001号"
+}
+})
